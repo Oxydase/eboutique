492cd1fb22d0152a05adbd3bb843df8d4271332a
Oxydase <figueiredo.milton16@gmail.com>
2025-05-07 16:18:21 +0200

    ajout controller category

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\CategoryRepository;
+
+
+final class CategoryController extends AbstractController
+{
+    #[Route('/categories', name: 'category_index')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+}
