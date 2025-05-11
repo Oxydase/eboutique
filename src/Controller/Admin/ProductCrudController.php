bfa27e1e6b2a76a23d81912a8008084f1175f9be
Oxydase <figueiredo.milton16@gmail.com>
2025-05-11 13:20:51 +0200

    modif productController

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
+use App\Entity\Category; // Assurez-vous d'importer l'entité Category
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField; // Importation du champ AssociationField
 
 class ProductCrudController extends AbstractCrudController
 {
@@ -15,14 +17,16 @@ class ProductCrudController extends AbstractCrudController
         return Product::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
+            TextField::new('name'), // Ajouter les autres champs ici selon ta table Product
             TextEditorField::new('description'),
+            TextField::new('price_ht'),
+            TextField::new('available'),
+            AssociationField::new('category') // Ajouter le champ category ici pour la sélection de la catégorie
+                ->setFormTypeOption('choice_label', 'name') // Choisir le champ 'name' pour afficher les catégories dans la liste déroulante
         ];
     }
-    */
 }
